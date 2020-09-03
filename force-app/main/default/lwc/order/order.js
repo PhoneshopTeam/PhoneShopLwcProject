84726f9e1ba6d4fc190d5b54f11dcc3700266b4c
Margarita <m.kumishcha@gmail.com>
2020-09-03 10:02:17 +0300

    modify showPositionInOrder

--- force-app/main/default/lwc/order/order.js
+++ force-app/main/default/lwc/order/order.js
@@ -23,7 +23,7 @@ import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
 import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import ORDER_NUMBER_FIELD from '@salesforce/schema/Custom_Order__c.Order_Number__c';
-// import TOTAL_AMOUNT_FIELD from '@salesforce/schema/Custom_Order__c.Total_Amount__c';
+import TOTAL_AMOUNT_FIELD from '@salesforce/schema/Custom_Order__c.Total_Amount__c';
 
 // const COLS = [{
 //         label: "Picture",
@@ -58,13 +58,10 @@ export default class Order extends NavigationMixin(LightningElement) {
     date;
     typeOfPayment;
     isHideDeliveryDate = false;
-    // isHidePositionsInOrder = false;
-    // labelOfPositionsButton = "Show all positions";
 
     fields = [FIRST_NAME_FIELD, LAST_NAME_FIELD, PHONE_FIELD, EMAIL_FIELD];
-    fieldsOfOrder = [ORDER_NUMBER_FIELD];
+    fieldsOfOrder = [ORDER_NUMBER_FIELD, TOTAL_AMOUNT_FIELD];
 
-    // columns = COLS;
     @wire(CurrentPageReference)
     currentPageReference;
 
@@ -86,10 +83,6 @@ export default class Order extends NavigationMixin(LightningElement) {
 
     @wire(getTypesOfPaymentOptions)
     typeOfPaymentOptions;
-
-    // @wire(getMobilesInOrder, {
-    //     orderId: "$orderId"
-    // }) mobiles;
 
     @wire(getDeliveryAdress, {
         contactId: "$contactId"
@@ -134,12 +127,6 @@ export default class Order extends NavigationMixin(LightningElement) {
 
     handleChange(event) {
         switch (event.target.dataset.id) {
-            // case "positions":
-            //     this.isHidePositionsInOrder = this.isHidePositionsInOrder ? false : true;
-            //     this.labelOfPositionsButton = this.isHidePositionsInOrder ?
-            //         "Hide all positions" :
-            //         "Show all positions";
-            //     break;
             case "address":
                 this.selectedAddressId = event.detail.value;
                 break;
--- force-app/main/default/lwc/showPositionsInOrder/showPositionsInOrder.js
+++ force-app/main/default/lwc/showPositionsInOrder/showPositionsInOrder.js
@@ -3,37 +3,92 @@ import {
     wire,
     api
 } from 'lwc';
-import getMobilesInOrder from "@salesforce/apex/MobileDataService.getMobilesInOrder";
+import getBasketsInOrder from "@salesforce/apex/MobileDataService.getBasketsInOrder";
 
 const COLS = [{
         label: "Picture",
-        fieldName: "Picture__c",
+        fieldName: "ProductId__r.Picture__c",
         hideDefaultActions: true,
         type: 'image'
     },
     {
         label: "Name",
-        fieldName: "Name",
+        fieldName: "ProductId__r.Name",
         hideDefaultActions: true
     },
     {
         label: "Price",
-        fieldName: "Price__c",
+        fieldName: "UnitPrice__c",
         hideDefaultActions: true,
         type: 'currency'
+    }, {
+        label: "Quantity",
+        fieldName: "Quantity__c",
+        hideDefaultActions: true,
     }
 ];
 
 export default class ShowPositionsInOrder extends LightningElement {
 
+    @api orderId;
     labelOfPositionsButton = "Show all positions";
     isHidePositionsInOrder = false;
+    basketsInOrder;
+    orderItems;
+    error;
     columns = COLS;
-    @api orderId;
 
-    @wire(getMobilesInOrder, {
+
+    @wire(getBasketsInOrder, {
         orderId: "$orderId"
-    }) mobiles;
+    })
+    wiredBaskets(result) {
+        console.log(' wiredBaskets');
+        this.basketsInOrder = result;
+        if (result.data) {
+            //  this is the final array into which the flattened response will be pushed.
+            let contactsArray = [];
+
+            for (let row of result.data) {
+                // this const stroes a single flattened row.
+                const flattenedRow = {}
+
+                // get keys of a single row — Name, Phone, LeadSource and etc
+                let rowKeys = Object.keys(row);
+
+                //iterate
+                rowKeys.forEach((rowKey) => {
+
+                    //get the value of each key of a single row. John, 999-999-999, Web and etc
+                    const singleNodeValue = row[rowKey];
+
+                    //check if the value is a node(object) or a string
+                    if (singleNodeValue.constructor === Object) {
+
+                        //if it's an object flatten it
+                        this._flatten(singleNodeValue, flattenedRow, rowKey)
+                    } else {
+
+                        //if it’s a normal string push it to the flattenedRow array
+                        flattenedRow[rowKey] = singleNodeValue;
+                    }
+
+                });
+
+                //push all the flattened rows to the final array
+                contactsArray.push(flattenedRow);
+            }
+
+            //assign the array to an array that's used in the template file
+            this.orderItems = contactsArray;
+
+        } else if (result.error) {
+            this.error = result.error;
+            this.orderItems = undefined;
+            console.log('error====>');
+        }
+    }
+    //  mobiles;
 
     handleChange() {
 
